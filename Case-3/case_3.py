b91863af58e39471d55ae22c3e5af07b97257963
Hanna <hannajaganjac@gmail.com>
2023-03-22 14:54:47 +0100

    HANNA

--- Case-3/case_3.py
+++ Case-3/case_3.py
@@ -1,5 +1,7 @@
 import case_3_api as c3api
 
+import laadpaal_statistiek
+
 print(c3api.get_RDW_brandstof(100).head())
 print(c3api.get_RDW_kenteken_df(100).head())
 print(c3api.get_OCM_df(100).head())
